364a8be8039b2cdce36789954e6734ae73712fc8
Joao Pedro Nascimento <nascjp.dev@gmail.com>
2026-03-15 12:29:43 -0300

    feat: adiciona métodofindByEmail no UsuarioRepository

--- src/main/java/br/com/financas/repository/UsuarioRepository.java
+++ src/main/java/br/com/financas/repository/UsuarioRepository.java
@@ -2,6 +2,13 @@ package br.com.financas.repository;
 
 import br.com.financas.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+  Optional<UserDetails> findByEmail(String email);
 }
