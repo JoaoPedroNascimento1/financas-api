f125335459929003e324d6135e80044cb352a51a
Joao Pedro Nascimento <nascjp.dev@gmail.com>
2026-03-15 12:25:31 -0300

    feat: implementa UserDetails na classe UsuÃario

--- src/main/java/br/com/financas/entity/Usuario.java
+++ src/main/java/br/com/financas/entity/Usuario.java
@@ -2,13 +2,18 @@ package br.com.financas.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Builder
 @Entity
 @Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Usuario {
+public class Usuario implements UserDetails {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,4 +27,39 @@ public class Usuario {
 
   @Column(nullable = false)
   private String senha;
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return List.of();
+  }
+
+  @Override
+  public String getPassword() {
+    return senha;
+  }
+
+  @Override
+  public String getUsername() {
+    return email;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
 }
